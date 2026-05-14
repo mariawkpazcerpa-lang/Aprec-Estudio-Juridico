de64b5430daf7b1382ec3fa7b7e0c3f6a9043c50
María de la Paz Cerpa <mariawkpazcerpa@gmail.com>
2026-05-14 00:33:12 -0300

    media queries y flatpickr

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://bxmkujpmpgxuhwtrzzkh.supabase.co'
 const supabaseKey = 'sb_publishable_FChtjqrdc8gDjFgpORbZBQ_3EOP30n-'
@@ -14,15 +14,22 @@ if (formulario) {
     formulario.addEventListener('submit', async (e) => {
         e.preventDefault(); // Evitamos que la página se recargue
 
+        const fechaValue = document.getElementById('turno-fecha').value;
+        const horaValue = document.getElementById('turno-hora').value;
+
         // Extraemos los datos de los inputs
         const datosConsulta = {
             name: document.getElementById('name').value,
             email: document.getElementById('email').value,
             asunto: document.getElementById('asunto').value,
             message: document.getElementById('message').value,
+        
+            turno_fecha: fechaValue ? fechaValue : null,
+            turno_hora: horaValue ? horaValue : null,
     
         };
         alert("Enviando datos...");
+
 
         console.log("Enviando datos a la base de datos...", datosConsulta);
         
--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
+
 function irAContacto() {
   const seccion = document.getElementById('contacto');
   if (seccion) {
     seccion.scrollIntoView({ behavior: 'smooth' });
   }
 }
+const toggleBtn = document.querySelector('.menu-hamburguesa');
+const navBar = document.querySelector('.navbar-collapse');
+
+console.log("Botón encontrado:", toggleBtn);
+console.log("Contenedor encontrado:", navBar);
+
+ if (toggleBtn && navBar) {
+
+    toggleBtn.addEventListener('click', (evento) => {
+      evento.preventDefault(); // Evitamos el comportamiento por defecto del botón
+      console.log("3. ¡Hiciste clic en el botón!");
+
+      navBar.classList.toggle('show');
+
+      console.log("4. Clase 'show' toggled en el navbar:", navBar.classList.contains('show') ? "Sí" : "No");
+    });
+  } else {
+    console.error("No se encontraron el botón o el contenedor del navbar. Verifica los selectores.");
+  }
+
+
+
+
+flatpickr("#turno-fecha",{
+    dateFormat: "Y-m-d",
+      disable: [
+        function(date) {
+            return (date.getDay() === 0 || date.getDay() === 6);
+            }
+        ],
+        minDate: "today", 
+
+    });
+flatpickr("#turno-hora", {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    minTime: "09:00",
+    maxTime: "18:00"
+});
